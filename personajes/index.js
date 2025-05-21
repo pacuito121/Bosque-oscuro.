8ba682c2149a44d8f8955f737bbf5d420ef56b9a
pacuito121 <corexmartex@gmail.com>
2025-05-21 09:57:07 -0600

    Add files via upload

---
+++ personajes/index.js
@@ -0,0 +1,31 @@
+var characters = {
+    "personas":[
+        {
+            "Clase":"Principal",
+            "Foto":"He.png",
+            "Nombre":"'Lobo'",
+            "Edad":"???",
+            "Altura":"???",
+            "Descripcion":"Ente con forma clara de lobo, pero con ligeros detalles que dan entender que no es solo un animal.",
+            "Comentarios":"Esa cosa no es simplemente un lobo..."
+        },
+        {
+            "Clase":"Principal",
+            "Foto":"Delia.png",
+            "Nombre":"Delia Groves",
+            "Edad":"11",
+            "Altura":"1.59m",
+            "Descripcion":"Soy yo. Una joven que fue ha explorar y que se separo de todos los demas, atrapandome a mi misma con ese ser.",
+            "Comentarios":"Solo soy yo, la tonta que me condeno a esta tortura"
+        },
+        {
+            "Clase":"Secundario",
+            "Foto":"X.png",
+            "Nombre":"Marcus Rives",
+            "Edad":"13",
+            "Altura":"1.67m",
+            "Descripcion":"Joven adolescente que se encontraba en el oscuro bosque al mismo tiempo que Delia.No era consciente de la presencia del lobo, por eso al escuchar el llanto de la chica comenzo a llamarla en voz alta de forma imprudente.",
+            "Comentarios":"Porque estaba el ahi?"
+        }
+    ]
+}
