63493cde495cdbc652d44994cea3880cffbceaec
pacuito121 <corexmartex@gmail.com>
2025-05-21 09:57:31 -0600

    Add files via upload

---
+++ objetos/index.js
@@ -0,0 +1,32 @@
+var items={
+    "item":[
+        {
+            "Nombre":"Papeles",
+            "ImgUrl":"Papel.png",
+            "Descripcion":"Trozo de papel no demasiado extenso tirado en la cabaña. Dentro de esta, habian varios trozos de papel regados, algunos en peor estado que otro.",
+            "Ubicacion":"Cabaña",
+            "Observacion":'"A considerar por su estado, eso quiere decir que no ha pasado mucho desde que alguien estuvo aqui antes."',
+        },
+        {
+            "Nombre":"Palo",
+            "ImgUrl":"Palo.png",
+            "Descripcion":"Una larga rama de uno de los arboles de los alrededores.",
+            "Ubicacion":"Bosque oscuro",
+            "Observacion":'"Nunca pense que un palo en un bosque podria ser tan conveniente."',
+        },
+        {
+            "Nombre":"Cerillo",
+            "ImgUrl":"Cerillos.png",
+            "Descripcion":"Cerillo dentro de su caja en aparente buen estado.",
+            "Ubicacion":"Cabaña",
+            "Observacion":'"Esta demasiado bien conservado como para que sea de hace años, o eso creo"',
+        },
+        {
+            "Nombre":"Fuego",
+            "ImgUrl":"Fuego.png",
+            "Descripcion":"Es el producto de la union del cerillo, palo y papel. Sirve como ultima opcion de armamento para enfrentar a la bestia",
+            "Ubicacion":"Bosque oscuro",
+            "Observacion":'"Probablemente sea lo unico que me pueda salvar"',
+        }
+    ]
+}
