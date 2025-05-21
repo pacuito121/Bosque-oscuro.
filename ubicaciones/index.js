0a097d7c9efd0eea2dbe2a42f851b9d618e0eda9
pacuito121 <corexmartex@gmail.com>
2025-05-21 09:04:45 -0600

    Add files via upload

---
+++ ubicaciones/index.js
@@ -0,0 +1,16 @@
+var ubic ={
+    "ubicaciones":[
+        {
+            "name":"Cabaña",
+            "img":"Cabaña.jpeg",
+        },
+        {
+            "name":"Bosque oscuro",
+            "img":"Bosque.jpeg",
+        },
+        {
+            "name":"Bosque (Incendio)",
+            "img":"Arder.gif",
+        }
+    ]
+}
